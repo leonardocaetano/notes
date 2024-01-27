4880715e9a6753223b07cc93cedf9ffbaad7ae88
Leonardo Caetano <leonardocaetano27@gmail.com>
2024-01-27 14:42:46 -0300

    Chapters 01 and 02 of the new python book is completed.

---
+++ 00_python/book_02.py
@@ -0,0 +1,29 @@
+# this is a start of a new book on python and programming in general. 
+
+# ====== 01 - starting with python
+# human language is too confusing for a computer to understand, although LLMs are changing that. 
+# python has this IDLE, but we can ignore it and just use this
+
+# ====== 02 - python and programming
+# well... programming has a lot to do with organizing. when dealing with a client who has a solution to a problem, 
+# you need first to really understand what the client wants. and only than you can type precise instructions to a computer
+# the point being: have a damn spec. 
+# there's also the point that programmers who can communicate well gets more opportunities and money and etc. true to some extend
+
+# and yes, you can process data with python...
+
+# yes. believe or not. python has functions.
+
+# this outputs the ascii (utf-8) value of the expression in decimal
+# this ord function expects a single char as the input
+print(ord('w'))
+print(ord('ç'))
+
+# chr is the opposite of ord
+print(chr(119))
+print(chr(231))
+
+# we do also have bin
+print(bin(1))
+print(bin(255))
+print(bin(256))
